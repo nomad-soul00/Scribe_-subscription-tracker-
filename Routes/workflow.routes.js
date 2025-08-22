e5ae2735a38cbf4bd09d37f994a4f0b83d92dd59
Nomad-soul00 <sahilahamed813@gmail.com>
2025-08-22 18:35:23 +0530

    worflow

--- Routes/workflow.routes.js
+++ Routes/workflow.routes.js
@@ -1,9 +1,8 @@
 import {Router} from 'express';
+import { sendRemainders } from '../controllers/workflow.controller';
 
 const workflowRouter = Router();
 
-workflowRouter.get('/', (req,res)=>{
-
-});
+workflowRouter.get('/', sendRemainders);
 
 export default workflowRouter;
--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -5,6 +5,8 @@ import Subscription from '../models/subscription.model.js';
 
 const {serve} = require('@upstash/workflow/express');
 
+const remainders = [7,5,2,1];   
+
 export const sendRemainders = serve(async (context)=>{
     const {subscriptionID } = context.requestPayload;
     const subscription = await fetchSubscription(context, subscriptionID);
@@ -15,10 +17,33 @@ export const sendRemainders = serve(async (context)=>{
 
     const renewalDate = dayjs(subscription.renewalDate);
     if(renewalDate.isBefore(dayjs())){
-        console.log(`renewal date has passed for subscription ${subscriptionID}`);
+        console.log(`renewal date has passed for subscription ${subscriptionID}. Stopping Workflow!!`);
+        return;
     }
-    
+
+    for(const daysBefore of remainders){
+         const remainderDate = renewalDate.subtract(daysBefore, 'day');
+
+         if(remainderDate.isAfter(dayjs())){
+            await sleepUntilReminder(context, `Remainder${daysBefore} days before`, remainderDate);
+         }
+
+         await triggerRemiander(context, `Remainder${daysBefore} days before`);
+    }
+
 });
+
+const sleepUntilReminder = async(context,label,date) =>{
+    console.log(`Sleeping until ${label} remainder date: ${date}`);
+    await context.sleepUntill(label, date.todate());
+
+}
+
+const triggerRemiander = async (context, label)=>{
+    return await context.run(label, ()=>{
+        console.log(`Triggering ${label} remainder now!!`);
+    })
+}
 
 const fetchSubscription= async(context, subscriptionID)=>{
     return await context.run('get subscription', ()=>{
